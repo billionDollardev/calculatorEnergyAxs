6dedce8d144df77c48c7dabef2ea0681d50b9cb1
billionDollardev <wialca.0511@gmail.com>
2024-08-14 14:41:07 -0500

    calculator Energy 2.0

---
+++ src/conteoenergias/ConteoEnergias.java
@@ -0,0 +1,15 @@
+
+package conteoenergias;
+
+import javax.swing.JFrame;
+
+
+public class ConteoEnergias {
+
+    
+    public static void main(String[] args) {
+        VntEnergia vntEnergia = new VntEnergia();
+        vntEnergia.setVisible(true);
+    }
+    
+}
